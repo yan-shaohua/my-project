f9f72f9499816d688a3cca47e5df290bdc9f69b9
YanShaoHua <p@ssw0rdYan>
2022-03-31 17:15:17 +0800

    配置多环境启动程序

--- src/main/java/com/xxoo/StartApplication.java
+++ src/main/java/com/xxoo/StartApplication.java
@@ -1,13 +1,25 @@
 package com.xxoo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StartApplication {
 
+    private static final Logger logger = LogManager.getLogger(StartApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(StartApplication.class, args);
+
+        String[] profiles = context.getEnvironment().getActiveProfiles();
+        for (String profile : profiles) {
+            logger.info("程序启动使用profile:{}",profile);
+        }
+
     }
 
 }
