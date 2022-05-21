7330e75a175472d1785eb73937e108cc3ce0bc22
闫绍华 <923596260@qq.com>
2022-05-21 23:19:20 +0800

    新增-读取文件指定行数内容

---
+++ src/main/java/com/xxoo/utils/ReadFileUtil.java
@@ -0,0 +1,155 @@
+package com.xxoo.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFileUtil {
+
+
+    public static void main(String[] args) {
+        //调用读取方法，定义文件以及读取行数
+        //readLastNLine(new File("/Users/yanshaohua/Desktop/闫绍华-Java开发-3年.txt"), 5L);
+        readFrontNLine(new File("/Users/yanshaohua/Desktop/闫绍华-Java开发-3年.txt"), 5L);
+    }
+
+
+    /**
+     * 读取文件前面N行
+     */
+    public static List<String> readFrontNLine(File file, long numRead) {
+        // 定义结果集
+        List<String> result = new ArrayList<String>();
+        //行数统计
+        long count = 0;
+
+        // 排除不可读状态
+        if (!file.exists() || file.isDirectory() || !file.canRead()) {
+            return null;
+        }
+
+        // 使用随机读取
+        RandomAccessFile fileRead = null;
+        try {
+            //使用读模式
+            fileRead = new RandomAccessFile(file, "r");
+            //读取文件长度
+            long length = fileRead.length();
+            //如果是0，代表是空文件，直接返回空结果
+            if (length == 0L) {
+                return result;
+            } else {
+                //初始化游标
+                long pos = 0;
+                while (pos < length) {
+                    pos++;
+                    //开始读取
+                    fileRead.seek(pos);
+                    //如果读取到\n代表是读取到一行
+                    if (fileRead.readByte() == '\n') {
+                        //使用readLine获取当前行
+                        String line = fileRead.readLine();
+                        //保存结果
+                        result.add(line);
+
+                        //打印当前行
+                        System.out.println(line);
+
+                        //行数统计，如果到达了numRead指定的行数，就跳出循环
+                        count++;
+                        if (count == numRead) {
+                            break;
+                        }
+                    }
+                }
+                if (pos == numRead) {
+                    fileRead.seek(numRead);
+                    result.add(fileRead.readLine());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileRead != null) {
+                try {
+                    //关闭资源
+                    fileRead.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    /**
+     * 读取文件最后N行
+     */
+    public static List<String> readLastNLine(File file, long numRead) {
+        // 定义结果集
+        List<String> result = new ArrayList<String>();
+        //行数统计
+        long count = 0;
+
+        // 排除不可读状态
+        if (!file.exists() || file.isDirectory() || !file.canRead()) {
+            return null;
+        }
+
+        // 使用随机读取
+        RandomAccessFile fileRead = null;
+        try {
+            //使用读模式
+            fileRead = new RandomAccessFile(file, "r");
+            //读取文件长度
+            long length = fileRead.length();
+            //如果是0，代表是空文件，直接返回空结果
+            if (length == 0L) {
+                return result;
+            } else {
+                //初始化游标
+                long pos = length - 1;
+                while (pos > 0) {
+                    pos--;
+                    //开始读取
+                    fileRead.seek(pos);
+                    //如果读取到\n代表是读取到一行
+                    if (fileRead.readByte() == '\n') {
+                        //使用readLine获取当前行
+                        String line = fileRead.readLine();
+                        //保存结果
+                        result.add(line);
+
+                        //打印当前行
+                        System.out.println(line);
+
+                        //行数统计，如果到达了numRead指定的行数，就跳出循环
+                        count++;
+                        if (count == numRead) {
+                            break;
+                        }
+                    }
+                }
+                if (pos == 0) {
+                    fileRead.seek(0);
+                    result.add(fileRead.readLine());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileRead != null) {
+                try {
+                    //关闭资源
+                    fileRead.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+
+        return result;
+    }
+}
